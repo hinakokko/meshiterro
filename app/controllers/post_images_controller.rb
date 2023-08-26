d4207f75ec4f9508fc7777df0a0c278995e1e411
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-08-26 13:45:59 +0000

    meshiterro_13章途中経過

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -10,6 +10,10 @@ class PostImagesController < ApplicationController
     @post_images = PostImage.all
   end
 
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -17,8 +21,11 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
-  def show
+  def destroy
     @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to '/show'
+
   end
 
   #投稿データのストロングパラメータ
