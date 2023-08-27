caa896e277d0bee5c78039082fed35f9a3e2d751
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-08-27 05:56:30 +0000

    meshiterro_15章途中経過

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -24,7 +24,7 @@ class PostImagesController < ApplicationController
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
-    redirect_to '/show'
+    redirect_to post_images_path
 
   end
 
