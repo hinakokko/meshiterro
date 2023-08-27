c42f4eafb279372566b3c8fc94e9dfae59979e4b
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-08-27 08:57:55 +0000

    meshiterro_16章途中経過

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   resources :post_images
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
   get 'homes/about' => 'homes#about', as: 'about'
 end
